a7e737053680f601a627e569b6eeb45456744941
Mumbi Chishimba <mumbi.chishimba@nipa.ac.zm>
2019-10-23 21:07:29 +0530

    - New fisp application

---
+++ src/main/java/zm/co/farmer/fisp/FispApplication.java
@@ -0,0 +1,13 @@
+package zm.co.farmer.fisp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FispApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FispApplication.class, args);
+	}
+
+}
---
+++ src/main/java/zm/co/farmer/fisp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package zm.co.farmer.fisp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FispApplication.class);
+	}
+
+}
